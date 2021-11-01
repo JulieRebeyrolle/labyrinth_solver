7d6dd106e6a9055676703d1afc797786f84c6086
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2021-11-01 20:53:39 +0100

    LabyrinthSolver done

--- src/LabyrinthSolver.php
+++ src/LabyrinthSolver.php
@@ -4,8 +4,62 @@ namespace App;
 
 class LabyrinthSolver
 {
-    public function isAccessible($height, $width, $map, $posX, $posY)
+
+    private array $map;
+    private int $height;
+    private int $width;
+    private array $accessibleFromStart = [];
+
+    public function isAccessible(int $height, int $width, array $map, int $posX, int $posY): bool
     {
+        $this->map = $map;
+        $this->height = $height;
+        $this->width = $width;
+
+        $accessibleCoords = $this->getAllAccessibleCoords();
+
+        $this->isAccessibleFromStart($posY, $posX);
+        sort($this->accessibleFromStart);
+        sort($accessibleCoords);
+
+        return $this->accessibleFromStart === $accessibleCoords ? 1 : 0;
     }
 
+    private function getAllAccessibleCoords(): array
+    {
+        $accessibleCoords = [];
+        for($y = 0; $y < $this->height; $y++) {
+            for($x = 0; $x < $this->width; $x++) {
+                if($this->map[$y][$x] === ' ') {
+                    $accessibleCoords[] = "$y-$x";
+                }
+            }
+        }
+
+        return $accessibleCoords;
+    }
+
+    private function isAccessibleFromStart(int $y, int $x): bool
+    {
+        if ($this->map[$y][$x] === 'X' || in_array("$y-$x", $this->accessibleFromStart)) {
+            return false;
+        }
+
+        $this->accessibleFromStart[] = "$y-$x";
+
+        if (
+            isset($this->map[$y][$x - 1]) && $this->isAccessibleFromStart($y, $x - 1)
+            || isset($this->map[$y][$x + 1]) && $this->isAccessibleFromStart($y, $x + 1)
+            || isset($this->map[$y - 1][$x]) && $this->isAccessibleFromStart($y - 1, $x)
+            || isset($this->map[$y + 1][$x]) && $this->isAccessibleFromStart($y + 1, $x)
+            || isset($this->map[$y + 1][$x - 1]) && $this->isAccessibleFromStart($y + 1, $x - 1)
+            || isset($this->map[$y + 1][$x + 1]) && $this->isAccessibleFromStart($y + 1, $x + 1)
+            || isset($this->map[$y - 1][$x - 1]) && $this->isAccessibleFromStart($y - 1, $x - 1)
+            || isset($this->map[$y - 1][$x + 1]) && $this->isAccessibleFromStart($y - 1, $x + 1)
+        ) {
+            return true;
+        }
+
+        return false;
+    }
 }
